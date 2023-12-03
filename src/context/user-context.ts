c6f9a372863c7c9dd8aaea9ec7227b6d304ce367
DariaMakh <makhortova.daria@ya.ru>
2023-12-03 10:49:43 +0300

    обновление

--- src/context/user-context.ts
+++
@@ -1,9 +0,0 @@
-import { createContext } from 'react';
-
-export interface UserContextType {
-	user: User;
-}
-
-export const UserContext = createContext<UserContextType | null>(null);
-
-UserContext.displayName = 'UserContext';
